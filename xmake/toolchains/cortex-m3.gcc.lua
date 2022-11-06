36136ad240b62e1f815b3a1929ed224c8bdb020c
xqyjlj <xqyjlj@126.com>
2022-11-06 14:54:19 +0800

    fix link error

--- xmake/toolchains/cortex-m3.gcc.lua
+++ xmake/toolchains/cortex-m3.gcc.lua
@@ -1,13 +1,16 @@
 set_xmakever("2.7.2")
 
-toolchain("arm-none-eabi") -- add toolchain
-    set_kind("cross") -- set toolchain kind
-	set_description("arm embedded compiler")
-toolchain_end()
-
-if(get_config("sdk")==nil) then
-    local default_path = "/opt/gcc-arm-none-eabi-10-2020-q4-major"
-    set_config("sdk", default_path) -- set toolchain directory
+function use_toolchain(sdk_path)
+    if(get_config("sdk") == nil) then
+        set_config("sdk", sdk_path) -- set toolchain directory
+    end
+    toolchain("arm-none-eabi") -- add toolchain
+        set_kind("cross") -- set toolchain kind
+        set_description("arm embedded compiler")
+        set_toolset("cc", "arm-none-eabi-gcc")
+        set_toolset("ld", "arm-none-eabi-gcc")
+        set_toolset("ar", "arm-none-eabi-ar")
+        set_toolset("as", "arm-none-eabi-gcc")
+    toolchain_end()
+    set_toolchains("arm-none-eabi") -- set toolchains
 end
-
-set_toolchains("arm-none-eabi") -- set toolchains
